92bab23080b4e9b8f53c70e5c868da33debcdcd9
tahakyo <ghazouani1988@email.com>
2022-05-17 19:02:21 +0200

    scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- header -->
+    <header>
+      <AppHeader />
+    </header>
+    <!-- main -->
+    <main>questo è main</main>
+    <!-- footer -->
+    <footer></footer>
   </div>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    AppHeader,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./style/common.scss";
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+<template>
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+</style>
