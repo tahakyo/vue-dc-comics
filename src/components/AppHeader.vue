2f71bc94834ae6c988c8521b1a0c39a2624cce74
tahakyo <ghazouani1988@email.com>
2022-05-17 20:47:43 +0200

    add header with static menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,24 @@
 /* eslint-disable prettier/prettier */
 <template>
-  <div>header</div>
+  <header class="container header">
+    <div class="header__logo">
+      <img src="../assets/img/dc-logo.png" alt="dc-logo" />
+    </div>
+    <nav>
+      <ul class="header__menu">
+        <li><a href="">link title</a></li>
+        <li><a href="">link title</a></li>
+        <li><a href="">link title</a></li>
+        <li><a href="">link title</a></li>
+        <li><a href="">link title</a></li>
+        <li><a href="">link title</a></li>
+        <li><a href="">link title</a></li>
+        <li><a href="">link title</a></li>
+        <li><a href="">link title</a></li>
+        <li><a href="">link title</a></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -11,4 +29,24 @@ export default {
 
 <style lang="scss" scoped>
 @import "../style/variables.scss";
+.header {
+  display: flex;
+  justify-content: space-between;
+  min-width: 1000px;
+  &__logo {
+    width: 5rem;
+    padding: 5px;
+  }
+  nav {
+    display: flex;
+    align-items: center;
+  }
+  &__menu {
+    display: flex;
+    li {
+      margin-right: 1rem;
+      color: black;
+    }
+  }
+}
 </style>
